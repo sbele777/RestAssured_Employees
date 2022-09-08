f58ecc643aa5f9a87bcd27acd3455e74462e36fa
desktop-edh5clb\owner <beles0960@gmail.com>
2022-09-08 17:48:20 +0530

    First Commit

---
+++ RestAssured_Framework/src/main/java/RestAssured_Framework/RestAssured_Framework/App.java
@@ -0,0 +1,13 @@
+package RestAssured_Framework.RestAssured_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
